de2d77d3c9e0d84eeb4c94089222b68e5d20c1f6
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-18 19:04:08 +0530

    html done(w/o navbar)

---
+++ app.js
@@ -0,0 +1,11 @@
+const angles=document.querySelectorAll(".angles-class");
+const submitbtn=document.querySelector(".submit-button");
+const ans=document.querySelector(".answer");
+
+
+function checktraingle(){
+    console.log("hello");
+}
+
+submitbtn.addEventListener("click", checktraingle)
+
