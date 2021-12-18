862b3d0ae517cb138c2c016f2331912dd0f1421a
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-18 19:36:35 +0530

    js is done

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const angles=document.querySelectorAll(".angles-class");
-const submitbtn=document.querySelector(".submit-button");
-const ans=document.querySelector(".answer");
+const angles=document.querySelectorAll('.angles-class');
+const submitbtn=document.querySelector('.submit-button');
+const ans=document.querySelector('.answer');
 
 
-function checktraingle(){
-    console.log("hello");
+function calsum(angel1,angel2,angel3){
+    const tempsum=angel1+angel2+angel3;
+    return tempsum;
 }
 
-submitbtn.addEventListener("click", checktraingle)
+function checktraingle(){
+
+    const sum=calsum( Number(angles[0].value),Number(angles[1].value),Number(angles[2].value));
+
+    if(sum===180){
+        ans.innerText="Traingle it is!!!";
+    }else{
+        ans.innerText="Heyy,its not a Traingle !!!";
+    }
+    
+}
+
+submitbtn.addEventListener("click", checktraingle);
 
